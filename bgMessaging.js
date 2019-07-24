c87b9e6cddab3a7c390597145b5ab069f30a6aeb
Akhilesh <akhilramkee123@gmail.com>
2019-07-24 14:13:12 +0530

    Version 1 - Not working

---
+++ bgMessaging.js
@@ -0,0 +1,8 @@
+import firebase from 'react-native-firebase';
+import type { RemoteMessage} from 'react-native-firebase';
+
+export default async (message: RemoteMessage) => {
+    // handle your message
+
+    return Promise.resolve();
+}
